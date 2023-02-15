952ba9b38204b6ac7073242d044651e20a598380
collo <mutaic84@gmail.com>
2023-02-15 23:27:27 +0300

    :hammer: files update

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+
+	"github.com/Ckm54/go-jwt/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+}
